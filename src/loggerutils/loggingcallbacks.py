fix load variance logging

src/loggerutils/loggingcallbacks.py
@@ -45,7 +45,7 @@ class LoggingCallbacks(DefaultCallbacks):
         episode.custom_metrics["srv/max_temp_cpu"] = env.servers.temp_cpu.max()
         episode.custom_metrics["srv/server_total_flow"] = np.sum(env.servers.flow)
         episode.custom_metrics["srv/overheated_inlets"] = env.servers.overheated_inlets
-        episode.custom_metrics["srv/load_variance"] = env.servers.load_variance
+        episode.custom_metrics["srv/load_variance"] = np.var(env.servers.load)
 
         for i in range(env.n_crah):
             episode.custom_metrics[f"crah{i}/temp_in"] = env.flowsim.crah_temp_in[i]