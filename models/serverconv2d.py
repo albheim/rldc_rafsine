6be5954a679a5bd4b0717c91338f3b1aeaa2ab0f
Albin Heimerson <albheim@hotmail.com>
2021-06-09 17:57:16 +0200

    Remake

--- models/serverconv2d.py
+++ models/serverconv2d.py
@@ -9,15 +9,25 @@ tf = tf2
 class ServerConv2DNetwork(TFModelV2):
     # https://docs.ray.io/en/master/rllib-models.html#more-examples-for-building-custom-models
 
-    def __init__(self, obs_space, action_space, num_outputs, model_config, name, 
-            n_servers=360, n_crah=4, n_hidden=256, inject=False, activation='relu',
-            n_conv_layers=1, n_conv_hidden=1, n_pre_layers=1, n_crah_layers=1, n_value_layers=2):
+    def __init__(self, obs_space, action_space, num_outputs, model_config, name, **kwargs):
         obs_space = obs_space.original_space if hasattr(obs_space, "original_space") else obs_space
         super(ServerConv2DNetwork, self).__init__(
             obs_space, action_space, num_outputs, model_config, name)
 
-        
         #fixed_input = tf.keras.layers.Input(tensor=tf.constant([1, 2, 3, 4]))
+
+        n_servers = kwargs.get("n_servers", 360)
+        n_crah = kwargs.get("n_crah", 4)
+        n_racks = kwargs.get("n_racks", 12)
+        n_hidden = kwargs.get("n_hidden", 256)
+        activation = kwargs.get("activation", 'relu')
+        n_conv_layers = kwargs.get("n_conv_layers", 1)
+        n_conv_hidden = kwargs.get("n_conv_hidden", 1)
+        n_pre_layers = kwargs.get("n_pre_layers", 1)
+        n_crah_layers = kwargs.get("n_crah_layers", 1)
+        n_value_layers = kwargs.get("n_value_layers", 2)
+        conv_filter_size = kwargs.get("conv_filter_size", 1)
+        rack_inject = kwargs.get("rack_inject", False)
 
         input_load = tf.keras.layers.Input(shape=(n_servers,))
         input_temp = tf.keras.layers.Input(shape=(n_servers,))
@@ -29,35 +39,33 @@ class ServerConv2DNetwork(TFModelV2):
 
         all_conc = tf.keras.layers.Concatenate()(other_inputs + server_inputs)
 
-        # Pre net, goes both to conv and crah 
-        pre = all_conc
-        for i in range(n_pre_layers):
-            pre = tf.keras.layers.Dense(n_hidden, activation=activation, name=f"pre_dense{i}")(pre)
-
-        # Conv input
-        server_inject = tf.keras.layers.Dense(n_servers, activation=activation)(all_conc)
-        if inject:
-            server_inputs += [server_inject]
-
-        # Job placement
-        filter_size=11
-        server_reshaped = [tf.keras.layers.Reshape(target_shape=(n_crah, -1, 1))(x) for x in server_inputs]
+        # Server inputs to racks * servers * params
+        server_reshaped = [tf.keras.layers.Reshape(target_shape=(n_racks, n_servers // n_racks, 1))(x) for x in server_inputs]
         server_concat = tf.keras.layers.Concatenate(name="server_concat")(server_reshaped)
+
+        # Rack based input layer, each server within a rack can be affected by other servers in same rack
+        if rack_inject:
+            conv_rack_inject = tf.keras.layers.Conv2D(n_servers // n_racks, (1, n_servers // n_racks), activation=activation, name="conv_rack_inject")(server_concat)
+            conv_rack_inject_reshape = tf.keras.layers.Reshape(target_shape=(n_racks, n_servers // n_racks, 1), name="conv_rack_inject_reshape")(conv_rack_inject)
+            server_concat = tf.keras.layers.Concatenate(name="conv_in_concat")([server_concat, conv_rack_inject_reshape])
+
         convlayer = server_concat
         for i in range(n_conv_layers):
-            convlayer = tf.keras.layers.Conv2D(n_conv_hidden, (1, filter_size), padding="same", activation=activation, name=f"server_conv{i}")(convlayer)
-        conv_server_out = tf.keras.layers.Conv2D(1, (1, filter_size), padding="same", activation=activation, name="server_conv_last")(convlayer)
+            convlayer = tf.keras.layers.Conv2D(n_conv_hidden, (1, conv_filter_size), padding="same", activation=activation, name=f"server_conv{i}")(convlayer)
+        conv_server_out = tf.keras.layers.Conv2D(1, (1, conv_filter_size), padding="same", activation=activation, name="server_conv_last")(convlayer)
         conv_flattened = tf.keras.layers.Flatten(name="server_out")(conv_server_out)
         placement_out = tf.keras.layers.Multiply()([conv_flattened, input_job]) # Removes gradient if zero job was placed
 
         # Rack based input
-        conv_rack_out = tf.keras.layers.Conv2D(1, (1, n_servers // n_crah), activation=activation, name="rack_conv")(convlayer)
-        rack_flattened = tf.keras.layers.Flatten(name="rack_conv_out")(conv_rack_out)
+        rack_mean_vals = tf.keras.layers.Lambda(lambda x: tf.keras.backend.mean(x, axis=2))(server_concat)
+        #conv_rack_out = tf.keras.layers.Conv2D(1, (1, n_servers // n_racks), activation=activation, name="conv_rack_for_crah")(server_concat)
+        rack_flattened = tf.keras.layers.Flatten(name="rack_conv_out")(rack_mean_vals)
+
+        # Server based inputs
+        server_mean_vals = tf.keras.layers.Lambda(lambda x: tf.keras.backend.mean(x, axis=1))(rack_mean_vals)
         
         # Crah settings
-        server_total_load = tf.keras.layers.Lambda(lambda x: tf.keras.backend.sum(x, axis=1, keepdims=True))(input_load)
-        server_mean_temp = tf.keras.layers.Lambda(lambda x: tf.keras.backend.mean(x, axis=1, keepdims=True))(input_temp)
-        crah_input = tf.keras.layers.Concatenate(name="crah_concat")([input_outdoor_temp, server_total_load, server_mean_temp, rack_flattened])
+        crah_input = tf.keras.layers.Concatenate(name="crah_concat")([input_outdoor_temp, server_mean_vals, rack_flattened])
         crah_dense = crah_input # all_conc
         for i in range(n_crah_layers):
             crah_dense = tf.keras.layers.Dense(n_hidden, activation=activation, name=f"crah_dense{i}")(crah_dense)
