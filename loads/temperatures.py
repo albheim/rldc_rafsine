fcb835b12ecf4f67d8d022c2b6954a17b96fe010
Albin Heimerson <albheim@hotmail.com>
2021-09-09 13:31:27 +0000

    random phase

--- loads/temperatures.py
+++ loads/temperatures.py
@@ -1,15 +1,14 @@
 import numpy as np
 
 class SinusTemperature:
-    def __init__(self, offset, amplitude, phase=0, noise=0, period=24*60*60):
+    def __init__(self, offset, amplitude, noise=0, period=24*60*60):
         self.period = period
         self.offset = offset
         self.amplitude = amplitude
-        self.phase = phase
         self.noise = noise
     def seed(self, seed):
         self.rng = np.random.default_rng(seed)
-        self.phase = self.rng.random() * 24 * 60 * 60
+        self.phase = self.rng.random() * self.period
     def __call__(self, t):
         return self.offset + self.amplitude * np.sin(2 * np.pi * (t - self.phase) / self.period) + self.noise * self.rng.normal()
     def min_values(self):
