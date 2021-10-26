d3dc7e752360efc80a82f71f9cb7ea045f833094
Albin Heimerson <albheim@hotmail.com>
2021-10-26 09:22:29 +0000

    add hour offset to match

--- loads/workloads.py
+++ loads/workloads.py
@@ -6,7 +6,7 @@ class RandomArrival:
         self.duration = duration
         self.p = p
         self.seed(37)
-    def seed(self, seed):
+    def seed(self, seed, hour_offset=0):
         self.rng = np.random.default_rng(seed)
     def __call__(self, t):
         if self.rng.random() < self.p:
