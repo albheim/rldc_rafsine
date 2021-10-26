dd38ebeadd57cd7dd751daf6cc3ce68761361837
Albin Heimerson <albheim@hotmail.com>
2021-10-26 13:54:55 +0000

    restructure

---
+++ run.py
@@ -0,0 +1,176 @@
+import os
+from datetime import datetime
+import numpy as np
+
+import ray
+import ray.tune as tune
+from ray.rllib.models import ModelCatalog
+
+from util.parsing import parse_all
+from util.loggingcallbacks import LoggingCallbacks 
+from dc.dc import DCEnv
+from models.serverconv import ServerConvNetwork
+from models.serverconv2d import ServerConv2DNetwork
+from models.emptynet import EmptyNetwork
+
+def run(
+    rafsine = False,
+    crah_out_setpoint = 22.0,
+    crah_flow_setpoint = 0.8,
+    n_bins = 0,
+    break_after = -1.0,
+
+    avg_load = 100.0,
+    job_p = 0.5,
+
+    model = "baseline",
+    alg = "PPO",
+    seed = 37,
+    tag = "default",
+    n_envs = 1,
+    timesteps = 500000,
+    n_samples = 1,
+    horizon = 200,
+    output = None,
+    hyperopt = False,
+
+    loglevel = 1,
+    verbose = 0,
+):
+    # Register env with ray
+    tune.register_env("DCEnv", DCEnv)
+
+    # Register model with ray
+    ModelCatalog.register_custom_model("serverconv", ServerConvNetwork)
+    ModelCatalog.register_custom_model("serverconv2d", ServerConv2DNetwork)
+    ModelCatalog.register_custom_model("baseline", EmptyNetwork)
+
+    # Some common config
+    tune_config = {
+        # Environment
+        "env": "DCEnv",
+        "env_config": {
+            "rafsine_flow": rafsine,
+            "seed": seed,
+            "baseline": model == "baseline",
+            "crah_out_setpoint": crah_out_setpoint,
+            "crah_flow_setpoint": crah_flow_setpoint,
+            "avg_load": avg_load,
+            "loglevel": loglevel,
+            "n_bins": n_bins,
+            "break_after": break_after,
+            "job_p": job_p,
+        },
+
+        # Worker setup
+        "num_workers": n_envs, # How many workers are spawned, ppo use this many envs and data is aggregated from all
+        "num_envs_per_worker": 1, # How many envs on each worker? Can be used to vectorize, probably same as num_workers?
+        "num_gpus_per_worker": 1 if rafsine else 0, # Only give gpu to rafsine
+        "num_cpus_per_worker": 1, # Does this make any difference?
+        "seed": seed,
+
+        # For logging (does soft_horizon do more, not sure...)
+        "callbacks": LoggingCallbacks,
+        "soft_horizon": True,
+        "no_done_at_end": True,
+        "horizon": horizon, # Decides length of episodes, should for now be same as rollout_frament_length
+        "output": output,
+
+        # Training
+        "train_batch_size": n_envs * horizon, # Collects batch of data from different workes, does min/max/avg over it and trains on it
+        "rollout_fragment_length": horizon, # How much data is colelcted by each worker before sending in data for training
+
+        # Data settings
+        #"observation_filter": "MeanStdFilter", # Test this
+        #"normalize_actions": True,
+    }
+    # Trainer specific configs
+    trainer_config = {
+        # Model
+        "model": {
+            "custom_model": model,
+            "custom_model_config": {
+                "n_servers": tune_config["env_config"].get("n_servers", 360),
+                "activation": "elu", #tune.choice(["relu", "tanh", "elu"]),
+                "n_hidden": 64, #tune.choice([32, 128, 512]),
+                "rack_inject": True, #tune.choice([True, False]), # If true, pre is injected into server conv
+                "conv_filter_size": 11, #tune.choice([1, 5, 11]),
+                "filter_size": 5,
+                "n_conv_layers": 1, #tune.choice([0, 1, 3]),
+                "n_conv_hidden": 3, #tune.choice([1, 3]),
+                "n_crah_layers": 1, #tune.choice([0, 1, 3]),
+                "n_value_layers": 2, #tune.choice([0, 1]),
+                "crah_input": "other", #tune.choice(["all", "other"]),
+                "value_input": "all", #tune.choice(["all", "other"]),
+            },
+        },
+
+        # Agent settings
+        "vf_clip_param": 1000.0, #tune.choice([1.0, 10.0, 100.0, 1000.0]), # Set this to be around the size of value function? Git issue about this not being good, just set high?
+        "entropy_coeff": 0, #tune.choice([0, 1]),
+        "kl_target": 0.01, #tune.choice([0.01, 0.001, 0.1]),
+        "clip_param": 0.3, #tune.choice([0.3, 0.03, 3]),
+
+    }
+
+    tune_config.update(trainer_config)
+
+    # Update specific configs for temporary testing
+    if hyperopt:
+        # tune_config["env_config"]["loglevel"] = 1 
+
+        # tune_config["lr"] = tune.grid_search([0.0001, 0.05])
+        tune_config["model"]["custom_model_config"]["rack_inject"] = tune.choice([True, False])
+        tune_config["model"]["custom_model_config"]["train_batch_size"] = tune.choice([200, 1000, 5000])
+        tune_config["model"]["custom_model_config"]["n_hidden"] = tune.choice([16, 64, 512])
+        tune_config["model"]["custom_model_config"]["conv_filter_size"] = tune.choice([3, 7, 11, 15])
+        tune_config["model"]["custom_model_config"]["n_conv_layers"] = tune.choice([1, 2, 3])
+        tune_config["model"]["custom_model_config"]["activation"] = tune.choice(["relu", "tanh", "elu"])
+
+
+    analysis = tune.run(
+        alg, 
+        config=tune_config,
+        stop={
+            "timesteps_total": timesteps,
+        }, 
+
+        # Logging directories
+        name=datetime.now().strftime("%y%m%d_%H%M%S_") + tag,
+        local_dir=os.path.join(os.path.expanduser("~"), "results", "DCEnv", "RAFSINE" if rafsine else "SIMPLE", alg, model),
+        trial_name_creator=lambda trial: "trial",
+
+        # Tuning
+        num_samples=n_samples,
+        metric="episode_reward_mean",
+        mode="max",
+
+        checkpoint_freq=1000, # 200 horizon means checkpoint at every 200k?
+        checkpoint_at_end=True,
+        verbose=verbose,
+    )
+    
+    return analysis
+
+
+if __name__ == "__main__":
+    # Init ray with all resources
+    # needs $ ray start --head --num-cpus=32 --num-gpus=10
+    ray.init(address="auto")
+
+    args = parse_all()
+
+    analysis = run(**vars(args))
+
+# best_trial = analysis.best_trial  # Get best trial
+# best_config = analysis.best_config  # Get best trial's hyperparameters
+# best_logdir = analysis.best_logdir  # Get best trial's logdir
+# best_checkpoint = analysis.best_checkpoint  # Get best trial's best checkpoint
+# best_result = analysis.best_result  # Get best trial's last results
+# best_result_df = analysis.best_result_df  # Get best result as pandas dataframe
+# 
+# dfs = analysis.trial_dataframes
+
+# print(best_trial)
+# print(best_config)
+# print(best_logdir)
---
+++ util/parsing.py
@@ -0,0 +1,33 @@
+import argparse
+
+def parse_all():
+    parser = argparse.ArgumentParser()
+
+    # Env settings
+    parser.add_argument("--rafsine", action="store_true", help="If flag is set the rafsine backend will be used, otherwise the simple simulation is used.")
+    parser.add_argument("--crah_out_setpoint", type=float, default=22)
+    parser.add_argument("--crah_flow_setpoint", type=float, default=0.8)
+    parser.add_argument("--n_bins", type=int, default=0)
+    parser.add_argument("--break_after", type=float, default=-1)
+
+    parser.add_argument("--avg_load", type=float, default=100)
+    parser.add_argument("--job_p", type=float, default=0.5, help="Probability that a job arrives each time instance.")
+
+    # Training settings
+    parser.add_argument("--model", type=str, default="serverconv2d")
+    parser.add_argument("--alg", type=str, default="PPO")
+    parser.add_argument("--seed", type=int, default=37, help="Seed used for everything, should make the simulations completely reproducible.")
+    parser.add_argument("--tag", type=str, default="default")
+    parser.add_argument("--n_envs", type=int, default=1) # envs for each ppo agent
+    parser.add_argument("--timesteps", type=int, default=500000)
+    #parser.add_argument("--resume", type=str, default="", help="String with path to run to resume.")
+    parser.add_argument("--n_samples", type=int, default=1)
+    parser.add_argument("--horizon", type=int, default=200)
+    parser.add_argument("--output", type=str, default=None)
+    parser.add_argument("--hyperopt", action="store_true")
+
+    # Other settings
+    parser.add_argument("--loglevel", type=int, default=1, help="0 no extra, 1 most stuff, 2 all stuff")
+
+    args = parser.parse_args()
+    return args
